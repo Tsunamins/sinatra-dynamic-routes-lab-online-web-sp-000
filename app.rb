a2ca311cb25e3945ba09d751c7c5e86b933b71be
Alison Reilly <reillyamr@gmail.com>
2019-04-19 15:05:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -39,7 +39,7 @@ class App < Sinatra::Base
   end 
   
   get '/:operation/:number1/:number2/' do
-    binding.pry
+    
     @operation = params[:operation]
     
     @number1 = params[:number1].to_i 
